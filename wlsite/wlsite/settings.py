d038ee56efa0c7cd69b50ac193f837e74173b474
gabriele <gabrielet.galluzzo@gmail.com>
2022-04-22 16:22:41 +0200

    debug

--- wlsite/wlsite/settings.py
+++ wlsite/wlsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = int(os.environ.get("DEBUG", default=0))
-DEBUG = TRUE
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
